00abb346fbfa1147c7d598223da612d4c77dd924
sahil <sahilfar135@gmail.com>
2023-09-22 19:04:21 +0530

    firebase added

---
+++ lib/firebase_options.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+/// Default [FirebaseOptions] for use with your Firebase apps.
+///
+/// Example:
+/// ```dart
+/// import 'firebase_options.dart';
+/// // ...
+/// await Firebase.initializeApp(
+///   options: DefaultFirebaseOptions.currentPlatform,
+/// );
+/// ```
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions web = FirebaseOptions(
+      apiKey: "AIzaSyC3uF1Xgbja7mMHORfLeHp1E4eONioemiY",
+      authDomain: "flexing-a499f.firebaseapp.com",
+      projectId: "flexing-a499f",
+      storageBucket: "flexing-a499f.appspot.com",
+      messagingSenderId: "543710167743",
+      appId: "1:543710167743:web:3bb68c1eeeaf2440687c0a",
+      measurementId: "G-DFT90GJB36",
+    databaseURL: "https://flexing-a499f-default-rtdb.firebaseio.com/"
+  );
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,60 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flexing/firebase_options.dart';
 import 'package:flexing/widget/rotating.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
 
 import 'main_screen.dart';
 
-void main() => runApp(MaterialApp(
-    title: 'Flutter Demo',
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      scaffoldBackgroundColor: Colors.white, // Background color
-      primaryColor: Colors.white, // Text color
-      colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo),
-      iconTheme: const IconThemeData(
-        color: Colors.white, // Icon color
-      ),
-    ),
-    home: FutureBuilder(
-      future: Future.delayed(const Duration(milliseconds: 1500)),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return BagStoreHomePage();
-        } else {
-          return Scaffold(
-            backgroundColor: Colors.white54,
-            body: AnimatedTopWidget(
-              child: Image.asset(
-                "assets/images/flexing_logo.png",
-                fit: BoxFit.cover,
-              )
-            ),
-            );
-        }
-      },
-    )));
+
+Future main() async{
+WidgetsFlutterBinding.ensureInitialized();
+if(kIsWeb){
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
+FirebaseDatabase.instance.reference().child("users").push().set({
+  "name": "John Smith",
+  "age": 30
+});
+runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white, // Background color
+          primaryColor: Colors.white, // Text color
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo),
+          iconTheme: const IconThemeData(
+            color: Colors.white, // Icon color
+          ),
+        ),
+        home: FutureBuilder(
+          future: Future.delayed(const Duration(milliseconds: 1500)),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return BagStoreHomePage();
+            } else {
+              return Scaffold(
+                backgroundColor: Colors.white54,
+                body: AnimatedTopWidget(
+                    child: Image.asset(
+                      "assets/images/flexing_logo.png",
+                      fit: BoxFit.cover,
+                    )
+                ),
+              );
+            }
+          },
+        ));
+  }
+
+
+}
